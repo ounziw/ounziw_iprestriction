fb254389fe80bc32b034a716de86299e80abbd75
Fumito Mizuno <ounziw@gmail.com>
2014-08-21 13:41:23 +0900

    no IP restriction for default setting

--- config/id_ip.config.php
+++ config/id_ip.config.php
@@ -2,5 +2,5 @@
 // For override, copy this file and put into your
 // local/apps/ounziw_iprestriction/id_ip.config.php
 return array(
-    '1' => array('127.0.0.1'),
+   //'1' => array('127.0.0.1'),
 );
