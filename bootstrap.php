d67c64744e906edb2a56263cf52f0f78aa54cdf8
Fumito Mizuno <ounziw@gmail.com>
2014-08-21 09:51:56 +0900

    add events ounziw_iprestriction_invalid. You can change the default process, when a user logins from not-allowed IP.

--- bootstrap.php
+++ bootstrap.php
@@ -7,13 +7,12 @@
  */
 namespace Ounziw\Iprestriction;
 
-\Event::register('admin.loginSuccess', 'disconnect_from_invalid_ip');
-\Event::register('admin.loginSuccessWithCookie', 'disconnect_from_invalid_ip');
+\Event::register('admin.loginSuccess', '\Ounziw\Iprestriction\disconnect_from_invalid_ip');
+\Event::register('admin.loginSuccessWithCookie', '\Ounziw\Iprestriction\disconnect_from_invalid_ip');
 
 // invalid ip user will be redirected
 function disconnect_from_invalid_ip() {
     \Config::load('ounziw_iprestriction::id_ip', 'id');
-
     // IP check class
     $class = new \Ounziw\Iprestriction\Model_Checkip();
     $class->setCheckArray(\Config::get('id'));
@@ -26,8 +25,15 @@ function disconnect_from_invalid_ip() {
 
     if (!$class->validateUser($user->user_id, \Input::ip()))
     {
-        \Log::warning('invalid access ID: ' . $user->user_id . ' IP: ' . \Input::ip());
-        \Nos\Auth::disconnect();
-        \Response::redirect('/');
+        // You can override the default process.
+        if (\Event::has_events('ounziw_iprestriction_invalid')) {
+            \Event::trigger('ounziw_iprestriction_invalid');
+        }
+        else // Do default
+        {
+            \Log::warning('invalid access ID: ' . $user->user_id . ' IP: ' . \Input::ip());
+            \Nos\Auth::disconnect();
+            \Response::redirect('/');
+        }
     }
 }
--- config/id_ip.config.php
+++ config/id_ip.config.php
@@ -1,4 +1,6 @@
 <?php
+// For override, copy this file and put into your
+// local/apps/ounziw_iprestriction/id_ip.config.php
 return array(
-    '1' => array('127.0.0.1','192.168.33.10'),
+    '1' => array('127.0.0.1'),
 );
